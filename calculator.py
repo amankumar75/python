933ce0cd924f5f31e02ed3336bc78eb50c007e27
amankumar75 <amankumar919921@gmail.com>
2025-11-29 17:34:12 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,8 @@
+a = float(input("Enter a : "))
+b = float(input("Enter b : "))
+
+print("addition of",a,"&",b,"will be :",a+b)
+print(a,"-",b,"will be :",a-b)
+print("multiplication of",a,"&",b,"will be :",a*b)
+print(a,"division",b,"will be :",a/b)
+print(a,"integer division",b,"will be :",a//b)
